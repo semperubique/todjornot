4f614bdf17744e533cfc38cdb5b00af381fb444c
semperubique <lastmurname@gmail.com>
2022-09-18 16:07:42 +0200

    Add javascript

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-window.addEventListene('keydown', function (e));
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if(!audio) { return; } // stop if nothing is pressed
+    else {
+        audio.currentTime = 0; // rewind to the start
+        audio.play();
+        key.classList.add('playing');
+    }
+}
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') { return; } //skip the event if it is not a transform
+    else { this.classList.remove('playing');}
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
